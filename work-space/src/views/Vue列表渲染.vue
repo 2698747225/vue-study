a640d17dbf0b21e4530e8f4940db5a7d6248f858
liulingyu <liulingyu@lechebang.com>
2019-08-22 15:46:10 +0800

    feat(*.vue)：vue列表渲染测试提交

--- work-space/src/views/Vue列表渲染.vue
+++ work-space/src/views/Vue列表渲染.vue
@@ -1,7 +1,11 @@
+<!-- 1、v-for循环数组循环对象
+     2、数组和对象的变化检测机制
+     3、v-for、v-if作用同一模板-->
 <template>
   <div>
     <split>v-for循环数组</split>
-    <!-- cli2.3以后eslint会检测v-for的每个模板都必须带有key，保证数组内数据顺序和dom相同 ，注意这个key必须是当前页唯一，因此即使在两个for循环内使用相同key也会报错，建议拼字符串-->
+    <!--
+    cli2.3以后eslint会检测v-for的每个模板都必须带有key，保证数组内数据顺序和dom相同 ，注意这个key必须是当前页唯一，因此即使在两个for循环内使用相同key也会报错，建议拼字符串-->
     <ul v-for="(item,index) in array" :key="'array'+index">
       <li>{{item.a}}</li>
     </ul>
@@ -11,7 +15,8 @@
       <li>{{item}}</li>
     </ul>
 
-    <!-- Vue的数组检测机制是把数组分为了两种方式，一种是变异方式，另外一种则是非变异方式，变异方式一般为数组push、pop等常规的修改操作，vue对其进行了包装监听。
+    <!-- 
+      Vue的数组检测机制是把数组分为了两种方式，一种是变异方式，另外一种则是非变异方式，变异方式一般为数组push、pop等常规的修改操作，vue对其进行了包装监听。
         而非变异则为其他的方式。直接修改数组的引用指针可以触发检测，但若使用list[index]=1方式是无法触发检测的
     -->
     <split>数组检测机制测试</split>
@@ -27,7 +32,8 @@
     <!-- 可以检测到 -->
     <button @click="$set(changeArr,2,'10')">调用vm.$set方法修改</button>
 
-    <!-- 对象检测和数组类似，但没有了push、pop等等检测封装，对于对象属性添加、删除无法检测，同样可以使用vm.$set方法 -->
+    <!--
+    对象检测和数组类似，但没有了push、pop等等检测封装，对于对象属性添加、删除无法检测，同样可以使用vm.$set方法-->
     <split>对象检测机制测试</split>
     <ul v-for="(item,idx) in forObj" :key="'forObj'+idx">
       <li>{{item}}</li>
@@ -39,6 +45,14 @@
     <button @click="delete forObj.a">删除属性a</button>
     <!-- 视图发生变化-->
     <button @click="$set(forObj,'d',4)">使用vm.$set方法添加</button>
+
+    <!-- 
+    v-for的优先级高于v-if，相当于一个遍历模板内条件渲染-->
+    <split>组件上v-for、v-if同时使用</split>
+    <ul>
+      <!-- 只显示1、2，     cli2.3以后eslint会对v-for和v-if在同一个模板使用报错-->
+      <!-- <li v-for="(item,idx) in arr" :key="'arr'+idx" v-if="item===1||item===2">{{item}}</li> -->
+    </ul>
   </div>
 </template>
 <script>
@@ -52,7 +66,8 @@ export default {
         b: 2,
         c: 3
       },
-      changeArr: [1, 2, 3]
+      changeArr: [1, 2, 3],
+      arr: [1, 2, 3, 4, 5]
     };
   },
   components: { split },
