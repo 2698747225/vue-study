2ab6610e633eac9be6a563895887116409f8a751
liulingyu <liulingyu@lechebang.com>
2019-09-06 14:54:20 +0800

    feat(*)：测试过滤器

---
+++ work-space/src/views/Vue过滤器.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <label>{{date|dateFormat('yyyy-mm-dd hh:mm:ss')}}</label>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      date: new Date()
+    };
+  },
+  filters: {
+    dateFormat(date, type) {
+      if (
+        date &&
+        (typeof date === "string" ||
+          typeof date === "number" ||
+          date instanceof Date)
+      ) {
+        const oriDate = new Date(date);
+        const year = oriDate.getFullYear();
+        const month = oriDate.getMonth() + 1;
+        const day = oriDate.getDate();
+        const hour = oriDate.getHours();
+        const minute = oriDate.getMinutes();
+        const second = oriDate.getSeconds();
+        let formatType = type.toLocaleLowerCase();
+        switch (formatType) {
+          case "yyyy-mm-dd hh:mm:ss":
+            return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+          case "yyyy-mm-dd":
+            return `${year}-${month}-${day}`;
+        }
+      } else {
+        return date;
+      }
+    }
+  }
+};
+</script>
