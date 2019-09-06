9ae698ecdd6db536a85426981a51f8fca5fa0888
liulingyu <liulingyu@lechebang.com>
2019-09-06 14:53:37 +0800

    feat(*)：JSX写法提交

---
+++ work-space/src/views/Vue渲染函数/Vue（JSX）/Vue（JSX）.vue
@@ -0,0 +1,44 @@
+<!-- 
+    1、content
+    2、props
+    3、slots
+    4、directives
+    5、components
+
+    jsx比较类似react语法，之后再回来学习
+    总结来说，写法上双括号换成单括号
+    {{user}}====>{this.user}
+    v-bind:name="name"====>name={this.name}
+-->
+<script>
+export default {
+  data() {
+    return {
+      user: "liulingyu"
+    };
+  },
+  props: ["title"],
+  render() {
+    return (
+      <div>
+        <split>JSX-content</split>
+        <label>{this.user}</label>
+        <split>JSX-props</split>
+        <label>{this.title}</label>
+        <split>JSX-slots</split>
+        <label>{this.$slots.first}</label>
+        <split>JSX-directives</split>
+        <input type="text" vModel={this.user} />
+      </div>
+    );
+  },
+  mounted() {
+    console.log(this.$slots.first);
+  }
+};
+</script>
+<style lang="less" scoped>
+label {
+  color: red;
+}
+</style>
---
+++ work-space/src/views/Vue渲染函数/Vue（JSX）/父组件.vue
@@ -0,0 +1,22 @@
+<script>
+import child from "./Vue（JSX）";
+export default {
+  components: {
+    child: resolve => {
+      resolve(child);
+    }
+  },
+  data() {
+    return {
+      title: "测试JSX父组件props"
+    };
+  },
+  render() {
+    return (
+      <child title={this.title}>
+        <template slot="first">普通slot插槽</template>
+      </child>
+    );
+  }
+};
+</script>
